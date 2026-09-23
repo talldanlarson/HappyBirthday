f2e0adb208b01baaed866243b03ea4c622caaf7b
Dan Larson <talldanlarson@gmail.com>
2026-09-23 09:05:44 -0500

    Add From Signature

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     GreetingText(
                         message = "Android",
+                        from = "Dan",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,11 +33,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Text(
         text = message,
         fontSize = 100.sp,
         lineHeight = 116.sp
+    )
+    Text(
+        text = from,
+        fontSize = 36.sp
     )
 }
 
@@ -46,6 +51,6 @@ fun GreetingText(message: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Dan!")
+        GreetingText(message = "Happy Birthday Dan!", from = "From Lily")
     }
 }
